73e385e13de30b7b9d004de5dc6628f6dd0e8a1b
bwaheed22 <bw761@nyu.edu>
2021-08-27 11:52:52 -0400

    update aggregate script

--- data/aggregate_daily.R
+++ data/aggregate_daily.R
@@ -3,33 +3,53 @@ library(dplyr)
 # read in data
 calls_df <- readr::read_csv("data/311_cleaned.csv")
 
-# aggregate to daily based on compliant_type --> could also do it based on agency
-calls_daily <- calls_df %>%  
-  mutate(date = lubridate::date(created_datetime)) %>% 
-  select(date, complaint_type) %>% 
-  group_by(date, complaint_type) %>% 
+# clean up names
+calls_df$complaint_type <- stringr::str_to_lower(calls_df$complaint_type)
+
+# select top agencies with > 25,000 calls (~17 calls per day)
+top_agencies <- calls_df %>% 
+  group_by(agency) %>% 
   tally() %>% 
+  arrange(desc(n)) %>%
+  filter(n > 50000) %>% 
+  pull(agency)
+
+# aggregate to daily based on compliant_type --> could also do it based on agency
+calls_daily <- calls_df %>% 
+  filter(agency %in% top_agencies) %>% 
+  mutate(date = lubridate::date(created_datetime)) %>% 
+  select(date, agency, complaint_type) %>% 
+  group_by(date, agency, complaint_type) %>% 
+  tally() %>% 
+  arrange(agency, desc(n)) %>% 
   ungroup()
 
-# create misc category for complaint_types under 1000 calls
-top_types <- calls_daily %>% 
-  group_by(complaint_type) %>% 
+# look at low frequency complaint types within each agency:
+low_complaints <- calls_daily %>% 
+  group_by(agency, complaint_type) %>% 
   summarise(n = sum(n)) %>% 
-  filter(n >= 10000) %>% 
-  pull(complaint_type)
+  filter(n < 14000) %>% select(-n) %>% 
+  mutate(is_low = TRUE)
 
-# for non top types, recode as misc.
-calls_daily$complaint_type <- if_else(calls_daily$complaint_type %in% top_types,
-                                      calls_daily$complaint_type,
-                                      "Misc")
-# aggregate the Misc type
+# re-label  low complaint types as other, and then aggregate by day:
 calls_daily <- calls_daily %>% 
-  group_by(date, complaint_type) %>% 
-  summarize(n = sum(n),
-            .groups = 'drop')
+  left_join(low_complaints, by = c('agency', 'complaint_type')) %>%
+  mutate(is_low = ifelse(is.na(is_low),FALSE,TRUE),
+         complaint_type = ifelse(is_low == TRUE, 'other', complaint_type)) %>% 
+  group_by(date, agency, complaint_type) %>% summarise(n = sum(n)) %>% ungroup()
 
-# clean up names
-calls_daily$complaint_type <- stringr::str_to_lower(calls_daily$complaint_type)
+
+calls_daily %>% filter(agency == 'DEP') %>% 
+  ggplot(., aes(x = date, y = n)) +
+  facet_wrap(agency~complaint_type, scales = "free") + 
+  geom_line() + 
+  theme_bw() +
+  labs(title = "Calls by Agency per day",
+       subtitle = "July 10, 2017 - July 10, 2021",
+       x = "NYC Agency",
+       y = "Number of Calls") + 
+  scale_y_continuous(labels = scales::comma)
+
 
 # write out
 readr::write_csv(calls_daily, 'data/311_cleaned_daily.csv')
