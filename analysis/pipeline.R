56faa6dd39d2246ae590055d968799702232a3ad
bwaheed22 <bw761@nyu.edu>
2021-09-15 16:50:06 -0400

    merging

--- analysis/pipeline.R
+++ analysis/pipeline.R
@@ -20,22 +20,53 @@ yesterday_data <- RSocrata::read.socrata(paste0(url, query, collapse = ""))
 yesterday_data <- clean_data(yesterday_data) #!! need to change column types in order for bind_rows to work
 
 # Read in historical clean data:
-historical_data <- readr::read_csv('data/311_cleaned.csv')
+# TODO: do we want to read all the data or just the aggregated data?
+#   all the data will be slow especially if we move this to a raspberry pi
+historical_data <- readr::read_csv('data/311_cleaned.csv') %>% 
+  select(created_datetime, agency, complaint_type)
 
 # Append yesterday's data to historical:
 calls_full <- dplyr::bind_rows(historical_data, yesterday_data)
 
 # Aggregate data to daily:
 calls_daily <- aggregate_daily(calls_full)
+calls_daily <- calls_daily %>% 
+  tsibble::as_tsibble(key = c('agency', 'complaint_type'), 
+                      index = 'date') %>% 
+  tsibble::fill_gaps() %>% 
+  tidyr::fill(n, .direction = "down")
 
-# Backtest and forecast based on optimal models:
-forecasts <- backtest_forecast(calls_full)
+# read in the dataframe denoting the optimal models
+best_models <- readr::read_csv('analysis/backtest/best_models.csv')
+
+# make the forecasts
+forecasts <- make_forecasts(calls_daily, best_models, h = 7)
 
 
-# Plot: ----
+# TODO: we need a way to manage the historical data + daily data (e.g. a running csv)
 
+# combine historical last 3 weeks with one week forecast
+last_four_weeks <- forecasts %>% 
+  select(date, agency, complaint_type, .mean, n) %>% 
+  tsibble::as_tsibble() %>% 
+  bind_rows(rename(calls_daily, .mean = n)) %>% 
+  filter(date >= (Sys.Date() - 21))
 
+# extract prediction intervals and set lower bound to 0
+last_four_weeks <- last_four_weeks %>% 
+  fabletools::as_fable(response = '.mean', distribution = 'n') %>% 
+  hilo(level = 80) %>%
+  mutate(lower_80 = `80%`$lower,
+         upper_80 = `80%`$upper) %>% 
+  select(-`80%`, -n) %>% 
+  mutate(.mean = if_else(.mean < 0, 0, .mean),
+         lower_80 = if_else(lower_80 < 0, 0, lower_80),
+         upper_80 = if_else(upper_80 < 0, 0, upper_80))
 
+# write out data to be included with shiny app
+readr::write_csv(last_four_weeks, 'data/forecasts_daily.csv')
 
+# TODO: need a way to deal with yesterday's lack of data: use previous forecast? and then update the following day?
 
-
+# plot it
+plot_ts(last_four_weeks, .agency = 'TLC', .complaint_type = 'taxi complaint')
