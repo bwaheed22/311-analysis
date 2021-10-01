12a41ae27eba4cb7ee556b28a0425cfa1b7e97c2
Joe Marlo <joe.marlo.09@gmail.com>
2021-10-01 10:48:55 -0400

    moved shiny csvs to seperate folder

--- 311_calls/global.R
+++ 311_calls/global.R
@@ -9,9 +9,9 @@ theme_set(theme_fivethirtyeight(base_size = 10,
                                 base_family = 'arial'))
 
 # Read in daily forecasts, best models, and yesterday's actuals data frames:
-forecasts_daily <- readr::read_csv('forecasts_daily.csv')
-best_models<- readr::read_csv('best_models.csv')
-yest_data <- readr::read_csv('yesterday_data.csv') %>% 
+forecasts_daily <- readr::read_csv('data/forecasts_daily.csv')
+best_models<- readr::read_csv('data/best_models.csv')
+yest_data <- readr::read_csv('data/yesterday_data.csv') %>% 
   mutate(complaint_type = tolower(complaint_type))
 
 # Get unique agency names and complaint types:
